c8f6e4469b2143e66458f9a80c9085b67f590e5d
Adham Mohamed Sabry Zineldin <mohalya3@gmail.com>
2024-10-24 18:57:01 +0300

    PROJECT DONE

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {RouterLink, RouterOutlet} from '@angular/router';
 import {NavComponent} from "./components/nav/nav.component";
 import {SingleProductComponent} from "./components/pages/single-product/single-product.component";
@@ -9,6 +9,7 @@ import {ProfileComponent} from "./components/account/profile/profile.component";
 import {SignupComponent} from "./components/account/signup/signup.component";
 import {VerificationComponent} from "./components/account/verification/verification.component";
 import {AuthService} from "../services/product/auth.service";
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -34,18 +35,38 @@ export class AppComponent implements OnInit {
   static email = 'mohalya3@gmail.com';
   static emailPassword = 'uike gqor nymo nmdb';
   domain: string = '';
+  usedDomain: string = 'maayn.ddns.net';
+  backendPort = '3000';
+  frontendPort = '4200';
+
   ipAddress: string = ' ';
 
 
-  constructor(private authService: AuthService) {
+  constructor(@Inject(DOCUMENT) private document: Document, private authService: AuthService) {
     console.log('AppComponent initialized. Current user:', authService.getCurrentUser());
+    this.setBaseHref();
 
   }
+
+  private setBaseHref(): void {
+    const base = this.document.querySelector('base');
+    this.domain = window.location.hostname;
+    if (base) {
+      if (this.domain === this.usedDomain) {
+        base.setAttribute('href', `/app${this.frontendPort}/`);
+      }
+    }
+  }
+
 
   ngOnInit(): void {
     // Get the domain
     this.domain = window.location.hostname;
-    AppComponent.api = `http://${this.domain}:3000/api`;
+    if (this.domain !== this.usedDomain) {
+      AppComponent.api = `http://localhost:/${this.backendPort}/api`;
+    } else {
+      AppComponent.api = `https://${this.domain}/app${this.backendPort}/api`;
+    }
     console.log('Current domain:', this.domain);
   }
 
